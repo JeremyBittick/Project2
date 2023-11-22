54781c129ce6c81f581c754157a4d146eb9dcd6a
JeremyBittick <90660391+JeremyBittick@users.noreply.github.com>
2023-11-22 10:21:16 -0500

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request
 import serial
+from serial.serialutil import SerialException
 
 app = Flask(__name__)
-ser = serial.Serial('/dev/ttyACM0', 9600)
+
+# Attempt to connect to the serial port
+try:
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+except SerialException:
+    print("Serial port error: Unable to open '/dev/ttyACM0'")
+    ser = None
 
 @app.route('/')
 def index():
@@ -11,11 +18,18 @@ def index():
 @app.route('/command', methods=['POST'])
 def command():
     key = request.form['key']
-    if key == 'left':
-        ser.write(b'L')
-    elif key == 'right':
-        ser.write(b'R')
+    if ser is not None:
+        if key == 'left':
+            ser.write(b'L')
+        elif key == 'right':
+            ser.write(b'R')
+    else:
+        print("Serial port not available.")
     return '', 204
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    try:
+        app.run(host='0.0.0.0', debug=True)
+    finally:
+        if ser is not None:
+            ser.close()
